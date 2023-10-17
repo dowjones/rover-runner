93eb633ff8f662333e23aaf9a5c62520df2a0a03
WolffRuoff <ethan.ruoff@dowjones.com>
2023-10-17 11:13:47 -0400

    Update to fulfill eslint

--- src/redis.ts
+++ src/redis.ts
@@ -5,7 +5,7 @@ export const startRedis = () => {
   detect(6379).then((_port: number) => {
     // _port is the next available port so if equal then server isn't running
     if (6379 === _port) {
-      let redisTerminal =
+      const redisTerminal =
         window.terminals.find((i: { name: string }) => i.name === `Redis`) || window.createTerminal(`Redis`);
       redisTerminal.sendText('redis-server');
     }
@@ -13,7 +13,7 @@ export const startRedis = () => {
 };
 
 export const stopRedis = () => {
-  let redisTerminal = window.terminals.find((i: { name: string }) => i.name === `Redis`);
+  const redisTerminal = window.terminals.find((i: { name: string }) => i.name === `Redis`);
   if (redisTerminal) {
     redisTerminal.sendText('\u0003');
     redisTerminal.dispose();
